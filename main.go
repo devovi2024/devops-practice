f42c67594afbda469a052dd2d9d96d1d5c88a7f8
devovi2024 <arfanhosenovi@gmail.com>
2026-08-26 21:26:45 +0600

    feature2 Changed FizzBuzz output format

--- main.go
+++ main.go
@@ -1,17 +1,19 @@
 package main
+
 import "fmt"
+
 func main() {
     fmt.Println("=== FizzBuzz Game ===")
-	for i := 1; i <= 100; i++ {
-		switch {
-		case i%15 == 0:
-			fmt.Print("FizzBuzz ")
-		case i%3 == 0:
-			fmt.Print("Fizz ")
-		case i%5 == 0:
-			fmt.Print("Buzz ")
-		default:
-			fmt.Print(i, " ")
-		}
-	}
+    for i := 1; i <= 100; i++ {
+        switch {
+        case i%15 == 0:
+            fmt.Printf("FizzBuzz ")
+        case i%3 == 0:
+            fmt.Printf("Fizz ")
+        case i%5 == 0:
+            fmt.Printf("Buzz ")
+        default:
+            fmt.Printf("%d ", i)
+        }
+    }
 }
